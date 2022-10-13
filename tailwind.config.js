4304f6fed7f08ed2408a471c5fae94028fd34954
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-10-13 15:00:31 -0300

    chore: add start colors from design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      blue_middle: '#1fa2e4',
+      blue_white: '#7ee0f0',
+      blue_dark: '#0c74bc',
+      green_middle: '#8cb49c',
+      green_light: '#c0e2cf',
+      white: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
